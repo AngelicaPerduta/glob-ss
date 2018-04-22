10a8370ce9c5c4a255e5becc6834812589d774ee
AngelicaPerduta <rsperduta@gmail.com>
2018-04-23 10:17:00 +1200

    cope PATH_INFO server inconsistency

--- glob-ss.php
+++ glob-ss.php
@@ -5,7 +5,7 @@
 	
 	// option: override default display id for multiple slide displays 
 	$v = array_key_exists('v', $_GET)? $_GET['v']:'slides';
-	// glob pattern and can include a path, but remove the leading '/'
+	// glob pattern and can include an additional path, but remove the leading '/'
 	$p = array_key_exists('PATH_INFO', $_SERVER)? substr($_SERVER['PATH_INFO'], 1): ($v.'/*.jpg');
 // echo "alert(\"p=$p\");\n";
 	$p = json_encode(glob($p, GLOB_BRACE+GLOB_MARK)); // mark directories and expand braces, {[pP][nN][gG],[jJ][pP][gG]}
@@ -15,7 +15,7 @@
 	$root = array_key_exists('HTTP_HOST', $_SERVER)? $_SERVER['HTTP_HOST']:$default_domain;
 	// concatenate avoids it evaluating as numeric
 	$root .= array_key_exists('PHP_SELF', $_SERVER)? $_SERVER['PHP_SELF']:'/';
-	$root = 'http://' . substr($root, 0, strrpos($root, '/')+1);
+	$root = 'http://' . substr($root, 0, strrpos($root, '/glob-ss.php')+1);
 // echo "alert(\"root=$root\");\n"; // diagnostic
 
 	// create a new object for this slideshow
---
+++ readme.txt
@@ -0,0 +1,4 @@
+For a working example, expand this zip file on your web serever
+then open the page slides-test.html from there in your web browser.
+
+Currently more explanations are in comments in slides-test.html file.
